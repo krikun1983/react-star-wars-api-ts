1157222531ab24859b40e563c830f53887ce04a6
@krikun1983 <akhmadeevst@gmail.com>
2021-08-21 13:44:29 +0400

    init: start project

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/scss/app.scss';
+
+const App = (): JSX.Element => <div>Hello</div>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
