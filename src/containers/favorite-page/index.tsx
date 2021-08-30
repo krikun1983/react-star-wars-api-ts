ab490fc3616494c456f3709dfe796505637aa4eb
@krikun1983 <akhmadeevst@gmail.com>
2021-08-30 20:22:55 +0400

    feat: add favorite-page

--- src/containers/favorite-page/index.tsx
+++ src/containers/favorite-page/index.tsx
@@ -1,14 +1,27 @@
+import PeopleList from 'components/people-page/people-list';
 import useTypeSelector from 'hooks/useTypeSelector';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { PeopleListState } from 'types/people-list';
 
 const FavoritePage = (): JSX.Element => {
+  const [people, setPeople] = useState<PeopleListState[]>([]);
   const { favoritePerson } = useTypeSelector(state => state.favoritePerson);
 
-  return (
-    <>
-      <div>Hello FavoritePage component</div>
-    </>
-  );
+  useEffect(() => {
+    const arrayPeople = Object.entries(favoritePerson);
+
+    if (arrayPeople.length) {
+      const res = arrayPeople.map(item => {
+        return {
+          id: item[0],
+          ...item[1],
+        };
+      });
+      setPeople(res);
+    }
+  }, []);
+
+  return <>{people.length ? <PeopleList people={people} /> : <h2 className="favorite-comment">No data</h2>}</>;
 };
 
 export default FavoritePage;
--- src/utils/network.ts
+++ src/utils/network.ts
@@ -22,13 +22,11 @@ const getApiResource = async (url: string): Promise<boolean | PeopleResultsBody 
   try {
     const response = await fetch(url);
     if (!response.ok) {
-      // console.log('Could not fetch', response.status);
       return false;
     }
     const body = await response.json();
     return body;
   } catch (error) {
-    // console.error(error.message);
     return false;
   }
 };
